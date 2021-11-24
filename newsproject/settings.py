36506c4642dccf9e38be6ff62bf7bcd2a32c8ac0
nisargptl <nisarg3000@gmail.com>
2021-11-24 18:46:12 +0530

    some changes

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -128,9 +128,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS=(BASE_DIR.child('static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 print(STATIC_ROOT)
